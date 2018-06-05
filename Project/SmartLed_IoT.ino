d78efffffa4e376601a3b7aafe0248e545d9fa47
Vitor Carlos Vieira Souza <vitorcarlosvieirasouza@MacBook-Air-de-Vitor.local>
2018-06-05 08:11:08 -0300

    Refatoracao do código do Projeto

--- Project/SmartLed_IoT.ino
+++ Project/SmartLed_IoT.ino
@@ -2,6 +2,8 @@
   Download desse projeto no GitHub
 https://github.com/gabrielkenji12/Projeto_SmartLamp
 
+Biblioteca utilizada BLynk- IFTTT
+
   Blynk is a platform with iOS and Android apps to control
   Arduino, Raspberry Pi and the likes over the Internet.
   You can easily build graphic interfaces for all your
@@ -22,23 +24,27 @@ https://github.com/gabrielkenji12/Projeto_SmartLamp
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-const int LDRPin = A0;  //define a pin for Photo resistor
-#define ledPin D1 //D1     //define a pin for LED
-int sensorValue = 0; //define variable off sensor
-int count=0; //define counting
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
+//Define o LDR
+const int LDRPin = A0;  
+//define o pino do LED
+#define ledPin D1 
+//define a variavel para do LDR
+int sensorValue = 0; 
+//define um contador
+int count=0; 
+// Utilize Api Key gerada pelo blynk.
 char auth[] = "54fdc59952b541b286382a20289fd95a";
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
+// Insira seu Wifi.
 char ssid[] = "iPhone";
+// Coloque a senha do Wifi
 char pass[] = "nawf1997";
 
 void setup()
 {
-  // Debug console
-  Serial.begin(9600);  //debugging  serial communcation
+  // Debugg do console
+  //debugging  serial communcation
+  Serial.begin(9600);  
   pinMode( ledPin, OUTPUT ); 
   Blynk.begin(auth, ssid, pass);
 }
@@ -46,10 +52,12 @@ void setup()
 void loop(){
   
   Blynk.run();
-  sensorValue=analogRead(A0);   //read  the value of the photoresistor.
-  Serial.println(sensorValue);  // value of the photoresistor to the serial monitor.
+  //Ler o valor do LDR
+  sensorValue=analogRead(A0); 
+  // Valor do LDR para o Monitor 
+  Serial.println(sensorValue);  
   Blynk.virtualWrite(ledPin,HIGH);
-  if(sensorValue<300){
+  if(sensorValue<100){
     if(count==0){
       Blynk.notify("Está escuro"); 
       count = 1;
